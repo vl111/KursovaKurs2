30716698201fb3451d8c0fda503a52eace9ee4f2
VLAD <VLAD@VLAD-PC>
2017-10-22 15:32:15 +0300

    abdabda

--- Kursova2kurs/src/bankSystem/IdAlreadyExistsExeption.java
+++ Kursova2kurs/src/bankSystem/IdAlreadyExistsExeption.java
@@ -1,6 +1,23 @@
 package bankSystem;
 
-@SuppressWarnings("serial")
 public class IdAlreadyExistsExeption extends Exception {
+
+	private static final long serialVersionUID = -6964631209751135286L;
+
+	public IdAlreadyExistsExeption() {
+		super();
+	}
+
+	public IdAlreadyExistsExeption(String arg0) {
+		super(arg0);
+	}
+
+	public IdAlreadyExistsExeption(Throwable arg0) {
+		super(arg0);
+	}
+
+	public IdAlreadyExistsExeption(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+	}
 
 }
